9a27130608dc32919abb2922b903105d4890d61a
jbrucker <jebrucker@gmail.com>
2022-10-17 14:13:07 +0700

    add make_pizza method

--- pizzashop.py
+++ pizzashop.py
@@ -1,12 +1,13 @@
-from pizza import Pizza
+from pizza import Pizza, Salad
 
 # This function shows a limitation on tool-assisted
 # refactoring in a dynamic language like Python.
 #
 # When you rename the Pizza getPrice method to get_price,
 # does it rename the method here?
-# - if no type annotation on the pizza parameter, maybe not
-# - if use type annotation ':Pizza' on the parameter, it should
+# - if no type hint on the pizza parameter, maybe not
+# - if you use type hint ':Pizza' on the parameter, it should
+
 
 def order_pizza(pizza):
     """Print a description of a pizza, along with its price."""
@@ -17,23 +18,33 @@ def order_pizza(pizza):
     if pizza.toppings:
         description += " pizza with "+ ", ".join(pizza.toppings)
     else:
-        description += " plain cheeze pizza"
+        description += " plain cheese pizza"
     print(f"A {description}")
-    print("Price:", pizza.getPrice())
+    print("Price:", pizza.getPrice(), "Baht")
+
+
+def make_pizza(size, *toppings):
+    """Build a pizza with optional toppings.
+
+    :param size: size of the pizza
+    :param toppings: optional names of toppings
+    :returns: a pizza (of course)
+    """
+    pizza = Pizza(size)
+    for topping in toppings:
+        pizza.addTopping(topping)
+    return pizza
 
 
 if __name__ == "__main__":
-    pizza = Pizza('small')
-    pizza.addTopping("mushroom")
-    pizza.addTopping("tomato")
-    pizza.addTopping("pinapple")
+    # small pizza with 3 toppings
+    pizza = make_pizza('small', 'mushroom', 'pineapple', 'tomato')
     order_pizza(pizza)
 
-    # a plain pizza
-    pizza2 = Pizza("medium")
+    # a plain medium pizza
+    pizza2 = make_pizza("medium")
     order_pizza(pizza2)
 
-    # pizza with only one topping
-    pizza3 = Pizza("large")
-    pizza3.addTopping("seafood")
+    # large pizza with only one topping
+    pizza3 = make_pizza("large", "seafood")
     order_pizza(pizza3)
